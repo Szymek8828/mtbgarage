40173c5ea959bfb7a03ea061e5b7344d3e105121
Szymek8828 <106557435+Szymek8828@users.noreply.github.com>
2025-03-16 22:15:14 +0100

    Update js1.js

--- js1.js
+++ js1.js
@@ -68,7 +68,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
 
         showSlide(); // Pokazujemy pierwsze zdjęcie od razu
-        setInterval(showSlide, 3500); // Zmiana co 2.5 sekundy
+        setInterval(showSlide, 4000); // Zmiana co 2.5 sekundy
     });
 
     // Add event listener to each menu item to close the menu on click
